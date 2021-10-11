bc1ac2229877f8bfa2fbc02d10dd0ed954a2c21f
Yoshito <yoshityan.ibatyan@gmail.com>
2021-10-11 22:41:30 +0900

    template.bladeにヘッダー、フッター実装

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -9,18 +9,59 @@
 </head>
 
 <body>
-    <div class="text-center mt-10">
-        <p class="text-xs">fontの大きさ</p>
-        <p class="text-sm">fontの大きさ</p>
-        <p class="text-base">fontの大きさ</p>
-        <p class="text-lg">fontの大きさ</p>
-        <p class="text-xl">fontの大きさ</p>
-        <p class="text-2xl">fontの大きさ</p>
-        <p class="text-3xl">fontの大きさ</p>
-        <p class="text-4xl">fontの大きさ</p>
-        <p class="text-5xl">fontの大きさ</p>
-        <p class="text-6xl">fontの大きさ</p>
-    </div>
+    <header class="text-gray-600 body-font">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
+                    stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+                    viewBox="0 0 24 24">
+                    <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                </svg>
+                <span class="ml-3 text-xl">BBS</span>
+            </a>
+            <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                <a class="mr-5 hover:text-gray-900">First Link</a>
+                <a class="mr-5 hover:text-gray-900">Second Link</a>
+                <a class="mr-5 hover:text-gray-900">Third Link</a>
+                <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+            </nav>
+            @guest
+                <button
+                    class="inline-flex items-center bg-blue-400 border-0 py-1 px-3 focus:outline-none hover:bg-blue-300 rounded text-white mt-4 md:mt-0">twitterログイン
+                </button>
+            @endguest
+        </div>
+    </header>
+
+    @yield('content')
+
+    <footer class="text-gray-600 body-font">
+        <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+            <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
+                    stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+                    viewBox="0 0 24 24">
+                    <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                </svg>
+                <span class="ml-3 text-xl">BBS</span>
+            </a>
+            <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">©
+                2021 BBS
+                <a href="https://twitter.com/Y0sh1tyan" class="text-gray-600 ml-1" rel="noopener noreferrer"
+                    target="_blank">@4410</a>
+            </p>
+            <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                <a class="ml-3 text-gray-500">
+                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        class="w-5 h-5" viewBox="0 0 24 24">
+                        <path
+                            d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z">
+                        </path>
+                    </svg>
+                </a>
+            </span>
+        </div>
+    </footer>
 </body>
 
 </html>
