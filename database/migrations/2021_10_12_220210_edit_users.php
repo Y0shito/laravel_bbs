5fda0e04e6645cb4276b18931aba3cd82f11995f
Yoshito <yoshityan.ibatyan@gmail.com>
2021-10-13 09:21:03 +0900

    usersテーブルのカラム削除と論理削除実装

---
+++ database/migrations/2021_10_12_220210_edit_users.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EditUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->softDeletes();
+            $table->dropColumn(['email', 'email_verified_at', 'password', 'updated_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+            $table->string('email', 255);
+            $table->timestamp('email_verified_at');
+            $table->string('password', 255);
+            $table->timestamp('updated_at');
+        });
+    }
+}
