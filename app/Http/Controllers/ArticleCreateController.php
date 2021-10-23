3fee0ba80331c0d061cae3904bcc9813c52333ff
Y0shito <75707750+Y0shito@users.noreply.github.com>
2021-10-23 22:21:08 +0900

    create.bladeのdraft削除

--- app/Http/Controllers/ArticleCreateController.php
+++ app/Http/Controllers/ArticleCreateController.php
@@ -29,29 +29,4 @@ class ArticleCreateController extends Controller
 
         return redirect()->route('preview');
     }
-
-    public function draft(ArticleRequest $request)
-    {
-        $title = Spaceremoval::spaceRemoval($request->title);
-        $body = Spaceremoval::spaceRemoval($request->body);
-
-        DB::beginTransaction();
-        try {
-            $article = Article::Create(
-                [
-                    'user_id' => Auth::id(),
-                    'title' => $title,
-                    'body' => $body,
-                    'public_status' => PublicStatus::CLOSE,
-                ]
-            );
-            DB::commit();
-            return redirect()->route('mypage');
-        } catch (\Exception $e) {
-            DB::rollback();
-            $error = $e->getMessage();
-            dd($error);
-            return back();
-        }
-    }
 }
--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -35,8 +35,6 @@
                             </div>
                         </div>
                         <div class="p-2 w-full flex align-center">
-                            <button formaction="{{ route('draft') }}"
-                                class="mx-auto text-white bg-gray-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg">下書き保存</button>
                             <button formaction="{{ route('preview') }}"
                                 class="mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">プレビュー</button>
                         </div>
