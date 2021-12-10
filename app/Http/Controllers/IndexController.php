e8348ade7cfba551fb56693f97dbe21ee97b3382
Y0shito <75707750+Y0shito@users.noreply.github.com>
2021-12-10 17:32:01 +0900

    Indexの記事取得時にローカルスコープ追加

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
     public function showIndex()
     {
-        $articles = Article::with('user')->paginate(10);
+        $articles = Article::openArticles()->with('user')->get();
         return view('index', compact('articles'));
     }
 }
