ae5ad580c0d65a548c31da9a58cbca8bcdbb7750
Y0shito <75707750+Y0shito@users.noreply.github.com>
2021-12-13 14:50:51 +0900

    ブックマーク処理時のカウントとトランザクション実装

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -4,32 +4,59 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use App\Models\Bookmark;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Exception;
 
 class BookmarkController extends Controller
 {
     public function add(Request $request)
     {
-        $bookmark = Bookmark::create(
-            [
-                'user_id' => Auth::id(),
-                'article_id' => $request->article_id
-            ]
-        );
+        $id = $request->article_id;
+        DB::beginTransaction();
+        try {
+            $bookmark = Bookmark::create(
+                [
+                    'user_id' => Auth::id(),
+                    'article_id' => $id
+                ]
+            );
+            $article = Article::find($id);
+            $article->timestamps = false;
+            $article->increment('bookmarks');
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollback();
+            $error = $e->getMessage();
+            dd($error);
+        }
         return back();
     }
 
     public function remove(Request $request)
     {
-        $bookmark = Bookmark::where(
-            [
-                'user_id' => Auth::id(),
-                'article_id' => $request->article_id
-            ]
-        )->delete();
+        $id = $request->article_id;
+        DB::beginTransaction();
+        try {
+            $bookmark = Bookmark::where(
+                [
+                    'user_id' => Auth::id(),
+                    'article_id' => $id
+                ]
+            )->delete();
+            $article = Article::find($id);
+            $article->timestamps = false;
+            $article->decrement('bookmarks');
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollback();
+            $error = $e->getMessage();
+            dd($error);
+        }
         return back();
     }
 }
