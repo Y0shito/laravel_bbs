292cd53c415a858c0e186d7c4dad0b18aa45a63d
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-01-30 18:40:33 +0900

    自分のページかどうかの判定と機能追加

--- app/Http/Controllers/UserPageController.php
+++ app/Http/Controllers/UserPageController.php
@@ -8,6 +8,8 @@ use App\Models\Article;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
+use App\Enums\PublicStatus;
+use Illuminate\Http\Request;
 
 class UserPageController extends Controller
 {
@@ -16,11 +18,36 @@ class UserPageController extends Controller
         $user = User::where('id', $id)->withCount(['userFollowers' => function (Builder $query) {
             $query->where('user_id', Auth::id());
         }])->first();
-        $articles = Article::openArticles()->where('user_id', $id)->with(['user', 'category'])
+
+        $query = Article::where('user_id', $id)->with(['user', 'category'])
             ->withCount(['bookmark' => function (Builder $query) {
                 $query->where('user_id', Auth::id());
-            }])->get();
+            }]);
+
+        if ((int)$id === Auth::id()) {
+            $articles = $query->get();
+        } else {
+            $articles = $query->openArticles()->get();
+        }
 
         return view('userpage', compact('user', 'articles'));
+    }
+
+    public function StatusOpen(Request $request)
+    {
+        Article::find($request->id)->update(['public_status' => PublicStatus::OPEN]);
+        return back();
+    }
+
+    public function statusClose(Request $request)
+    {
+        Article::find($request->id)->update(['public_status' => PublicStatus::CLOSE]);
+        return back();
+    }
+
+    public function articleDelete(Request $request)
+    {
+        Article::find($request->id)->delete();
+        return back();
     }
 }
