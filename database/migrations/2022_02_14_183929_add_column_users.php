6886097caf35f9de8945418f09eab054c16df0eb
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-02-14 18:47:44 +0900

    usersにカラム追加migration作成

---
+++ database/migrations/2022_02_14_183929_add_column_users.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('introduction', 200)->nullable()->after('created_at');
+            $table->string('url', 2000)->nullable()->after('introduction');
+            $table->integer('articles')->default(0)->after('url');
+            $table->integer('bookmarks')->default(0)->after('articles');
+            $table->integer('followings')->default(0)->after('bookmarks');
+            $table->integer('followers')->default(0)->after('followings');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['introduction', 'url', 'articles', 'bookmarks', 'followings', 'followers']);
+        });
+    }
+}
