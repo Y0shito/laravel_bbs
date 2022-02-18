7ad353fce863fd70f702a3632ee22fb64fc25b51
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-02-18 16:25:28 +0900

    bookmarkedからtotal_bookmarkedに

--- database/migrations/2022_02_14_183929_add_column_users.php
+++ database/migrations/2022_02_14_183929_add_column_users.php
@@ -18,8 +18,8 @@ class AddColumnUsers extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('introduction', 200)->nullable()->after('created_at');
             $table->string('url', 2000)->nullable()->after('introduction');
-            $table->integer('bookmarked')->default(0)->after('url');
-            $table->integer('followings')->default(0)->after('bookmarked');
+            $table->integer('total_bookmarked')->default(0)->after('url');
+            $table->integer('followings')->default(0)->after('total_bookmarked');
             $table->integer('followers')->default(0)->after('followings');
         });
     }
@@ -32,7 +32,7 @@ class AddColumnUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['introduction', 'url', 'bookmarked', 'followings', 'followers']);
+            $table->dropColumn(['introduction', 'url', 'total_bookmarked', 'followings', 'followers']);
         });
     }
 }
