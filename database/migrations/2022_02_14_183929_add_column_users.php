3f6c587cb3f7eaab1a084a1982887679a3e6ea84
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-02-18 16:06:24 +0900

    artclesカラム削除

--- database/migrations/2022_02_14_183929_add_column_users.php
+++ database/migrations/2022_02_14_183929_add_column_users.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,8 +18,7 @@ class AddColumnUsers extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('introduction', 200)->nullable()->after('created_at');
             $table->string('url', 2000)->nullable()->after('introduction');
-            $table->integer('articles')->default(0)->after('url');
-            $table->integer('bookmarked')->default(0)->after('articles');
+            $table->integer('bookmarked')->default(0)->after('url');
             $table->integer('followings')->default(0)->after('bookmarked');
             $table->integer('followers')->default(0)->after('followings');
         });
@@ -31,7 +32,7 @@ class AddColumnUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['introduction', 'url', 'articles', 'bookmarked', 'followings', 'followers']);
+            $table->dropColumn(['introduction', 'url', 'bookmarked', 'followings', 'followers']);
         });
     }
 }
