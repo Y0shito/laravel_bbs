93ffbae16027d940d8e8dd8ed34d5a2f0ced95e3
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-02-21 17:55:47 +0900

    テーブル表示の変更

--- resources/views/userfollowing.blade.php
+++ resources/views/userfollowing.blade.php
@@ -45,17 +45,32 @@
         </div>
         <div class="container px-5 py-8 mx-auto">
             @isset($user->userFollows)
-                <table class="table-auto">
-                    <tbody>
-                        @foreach ($user->userFollows as $follow)
-                            <tr>
-                                <td class="border px-4 py-2">
-                                    <a href="{{ route('userpage', ['id' => $follow->id]) }}">{{ $follow->name }}</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <div class="flex flex-col">
+                    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="inline-block py-2 min-w-full sm:px-6 lg:px-8">
+                            <div class="overflow-hidden shadow-md sm:rounded-lg">
+                                <table class="min-w-full">
+                                    <tbody>
+                                        @foreach ($user->userFollows as $follow)
+                                            <tr
+                                                class="border-b odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700 dark:border-gray-600">
+                                                <td
+                                                    class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                    <a
+                                                        href="{{ route('userpage', ['id' => $follow->id]) }}">{{ $follow->name }}</a>
+                                                </td>
+                                                <td
+                                                    class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                                                    {{ $follow->total_bookmarked }}
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             @endisset
         </div>
     </div>
