aa0c630a0613bdbd256029db38a2249f44e2e570
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-02-22 23:52:31 +0900

    headerデザイン微修正

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -9,8 +9,8 @@
 </head>
 
 <body>
-    <header class="text-gray-600 body-font">
-        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <header class="text-gray-600 body-font bg-white sticky top-0 shadow-md">
+        <div class="container mx-auto flex flex-wrap px-5 py-2 flex-col md:flex-row items-center">
             <a href="{{ route('index') }}" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
                     stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
