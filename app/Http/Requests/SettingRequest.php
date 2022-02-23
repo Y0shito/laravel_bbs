a31d5d43e7b89fa7e50393733a89e679b58ee7de
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-02-23 22:41:52 +0900

    UserSettingsのバリデーション完成

---
+++ app/Http/Requests/SettingRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SettingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'introduction' => ['nullable', 'max:200'],
+            'url' => ['nullable', 'url', 'max:2000'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'introduction.max' => '200文字以内で入力してください',
+            'url.url' => '有効なURLではありません',
+            'url.max' => '2000文字以内で入力して下さい',
+        ];
+    }
+}
