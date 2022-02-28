b736485d8484f7251c3ff82bfc07edfe2d8f7792
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-02-28 21:16:28 +0900

    フォロー/フォロワー両対応出来るように

--- resources/views/components/userslist.blade.php
+++ resources/views/components/userslist.blade.php
@@ -3,7 +3,7 @@
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                 <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                    @isset($user->userFollows)
+                    @isset($user->$which)
                         <table class="min-w-full divide-y divide-gray-200">
                             <thead class="bg-gray-50">
                                 <tr>
@@ -24,7 +24,7 @@
                                     </th>
                                 </tr>
                             </thead>
-                            @foreach ($user->userFollows as $follow)
+                            @foreach ($user->$which as $follow)
                                 <tbody class="bg-white divide-y divide-gray-200">
                                     <tr>
                                         <td class="px-6 py-2 whitespace-nowrap">
