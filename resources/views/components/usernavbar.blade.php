f1c2503c2865671ca2d3716cdfa865fe19233ae2
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-02-28 12:37:31 +0900

    URLによりCSS表示変更

--- resources/views/components/usernavbar.blade.php
+++ resources/views/components/usernavbar.blade.php
@@ -11,18 +11,29 @@
         <nav
             class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center">
             <a href="{{ route('userpage', ['id' => $user->id]) }}"
-                class="mr-1 text-white bg-gray-600 border-0 py-1 px-3 rounded">書いた記事</a>
+                class="{{ \Route::is('userpage') ? 'text-white bg-gray-600' : 'text-gray-900 hover:bg-gray-200' }} border-0 py-1 px-3 rounded mr-1">
+                書いた記事</a>
             <a href="{{ route('userBookmarks', ['id' => $user->id]) }}"
-                class="mr-1 text-gray-900 hover:bg-gray-200 border-0 py-1 px-3 rounded">ブックマークした記事</a>
+                class="{{ \Route::is('userBookmarks') ? 'text-white bg-gray-600' : 'text-gray-900 hover:bg-gray-200' }} border-0 py-1 px-3 rounded mr-1">ブックマークした記事</a>
             <a href="{{ route('userFollowing', ['id' => $user->id]) }}"
-                class="mr-1 text-gray-900 hover:bg-gray-200 border-0 py-1 px-3 rounded">フォロー</a>
+                class="{{ \Route::is('userFollowing') ? 'text-white bg-gray-600' : 'text-gray-900 hover:bg-gray-200' }} border-0 py-1 px-3 rounded mr-1">フォロー</a>
             <a href="{{ route('userFollowers', ['id' => $user->id]) }}"
-                class="mr-1 text-gray-900 hover:bg-gray-200 border-0 py-1 px-3 rounded">フォロワー</a>
+                class="{{ \Route::is('userFollowers') ? 'text-white bg-gray-600' : 'text-gray-900 hover:bg-gray-200' }} border-0 py-1 px-3 rounded mr-1">フォロワー</a>
             @if ($isMyPage)
                 <a href="{{ route('userSettings', ['id' => $user->id]) }}"
-                    class="mr-1 text-gray-900 hover:bg-gray-200 border-0 py-1 px-3 rounded">設定</a>
+                    class="{{ \Route::is('userSettings') ? 'text-white bg-gray-600' : 'text-gray-900 hover:bg-gray-200' }} border-0 py-1 px-3 rounded mr-1">設定</a>
             @endif
         </nav>
+        <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+            <a href="https://twitter.com/intent/user?user_id={{ $user->twitter_id }}" class="mr-4 text-gray-500">
+                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    class="w-5 h-5" viewBox="0 0 24 24">
+                    <path
+                        d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z">
+                    </path>
+                </svg>
+            </a>
+        </span>
         @if (Auth::check() and !($user->id === Auth::id()))
             <form method="POST">
                 @csrf
