eb071b78fd515b3dd571fa6dd0de4576b5441c40
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-03-25 12:21:42 +0900

    Requestを削除

--- app/Http/Controllers/UserFollowerController.php
+++ app/Http/Controllers/UserFollowerController.php
@@ -6,20 +6,19 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class UserFollowerController extends Controller
 {
-    public function showFollowersPage(Request $request)
+    public function showFollowersPage($id)
     {
         $user = User::with('userFollowers')
             ->withCount(['userFollowers' => function (Builder $query) {
                 $query->where('user_id', Auth::id());
-            }])->find($request->id);
+            }])->find($id);
 
-        $isMyPage = (int)$request->id === Auth::id();
+        $isMyPage = (int)$id === Auth::id();
 
         $followers = DB::table('follow_user as opponent')
             ->leftJoin('follow_user as myself', function ($join) {
@@ -28,7 +27,7 @@ class UserFollowerController extends Controller
                     ->where('myself.user_id', '=', Auth::id());
             })
             ->leftJoin('users', 'opponent.user_id', '=', 'users.id')
-            ->where('opponent.following_id', '=', $request->id)
+            ->where('opponent.following_id', '=', $id)
             ->select('opponent.id', 'opponent.user_id', 'users.name', 'users.total_bookmarked', 'users.followings', 'users.followers', 'myself.user_id as is_following')
             ->orderBy('opponent.id', 'asc')
             ->get();
