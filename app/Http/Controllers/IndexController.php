97472a918b85adf01cd4b3d8cfb28fe1ead325e1
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-03-25 18:35:17 +0900

    記事取得部分を共通化メソッドに置き換え

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,16 +5,12 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\Article;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
     public function showIndex()
     {
-        $articles = Article::openArticles()->with(['user','category'])->withCount(['bookmark' => function (Builder $query) {
-            $query->where('user_id', Auth::id());
-        }])->sortable()->paginate(5);
+        $articles = Article::getArticles()->sortable()->paginate(5);
         return view('index', compact('articles'));
     }
 }
