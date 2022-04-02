b8d1cd93939faaba1e8620af7c89a5bf32664b45
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-04-02 23:33:15 +0900

    自分のページかどうかでクエリ挿入を分岐

--- app/Http/Controllers/UserFollowingController.php
+++ app/Http/Controllers/UserFollowingController.php
@@ -17,10 +17,12 @@ class UserFollowingController extends Controller
         $isMyPage = (int)$id === Auth::id();
 
         $followings = DB::table('follow_user as opponent')
-            ->leftJoin('follow_user as myself', function ($join) {
-                $join->on('opponent.following_id', '=', 'myself.following_id')
-                    ->on('opponent.user_id', '<>', 'myself.user_id')
-                    ->where('myself.user_id', '=', Auth::id());
+            ->leftJoin('follow_user as myself', function ($join) use ($isMyPage) {
+                $join->on('opponent.following_id', '=', 'myself.following_id');
+                if ($isMyPage === false) {
+                    $join->on('opponent.user_id', '<>', 'myself.user_id');
+                }
+                $join->where('myself.user_id', '=', Auth::id());
             })
             ->leftJoin('users', 'opponent.following_id', '=', 'users.id')
             ->where('opponent.user_id', '=', $id)
