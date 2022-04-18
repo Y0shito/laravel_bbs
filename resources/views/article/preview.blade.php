03add2e7a470ee4d4711e2b5d7c8dabf357303e4
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-04-18 14:12:38 +0900

    擬似クラスdisabledの有効化

--- resources/views/article/preview.blade.php
+++ resources/views/article/preview.blade.php
@@ -23,9 +23,9 @@
                 </div>
                 <div class="p-2 w-full flex align-center">
                     <button formaction="{{ route('draft') }}"
-                        class="mx-auto text-white bg-gray-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg">下書き保存</button>
+                        class="mx-auto text-white bg-gray-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg disabled:bg-gray-200 disabled:cursor-not-allowed">下書き保存</button>
                     <button formaction="{{ route('completion') }}"
-                        class="mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">公開する</button>
+                        class="mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg disabled:bg-gray-200 disabled:cursor-not-allowed">公開する</button>
                 </div>
             </div>
         </form>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,8 @@ module.exports = {
 
     variants: {
         extend: {
-            opacity: ['disabled'],
+            backgroundColor: ['disabled'],
+            cursor: ['disabled'],
         },
     },
 
