84c052d7d29144ebb0efd042326ca52921ac0220
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-06-10 17:44:44 +0900

    ルートの変更

--- routes/web.php
+++ routes/web.php
@@ -30,17 +30,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__ . '/auth.php';
 
-Route::get('index', [IndexController::class, 'showIndex'])->name('index');
+Route::get('/', [IndexController::class, 'showIndex'])->name('index');
 
 // Twitterログイン
 Route::get('twitter-login', [UserController::class, 'login'])->name('twitter-login');
