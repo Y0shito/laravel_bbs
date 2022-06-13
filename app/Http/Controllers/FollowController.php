dffddc5dd2c3945dd8b9b7c843c21019afcad175
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-06-13 18:56:24 +0900

    try/catchとLog実装

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -1,36 +1,59 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class FollowController extends Controller
 {
     public function follow(Request $request)
     {
-        $user = User::find(Auth::id());
-        $user->userFollows()->attach($request->following_id);
-        $user->increment('followings');
+        DB::beginTransaction();
 
-        $opponent = User::find($request->following_id);
-        $opponent->increment('followers');
+        try {
+            $user = User::find(Auth::id());
+            $user->userFollows()->attach($request->following_id);
+            $user->increment('followings');
+
+            $opponent = User::find($request->following_id);
+            $opponent->increment('followers');
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollback();
+            Log::critical();
+        }
 
         return back()
-        ->with(['class' => 'text-blue-500 body-font bg-blue-100 shadow-md', 'message' => "{$opponent->name}さんをフォローしました"]);
+            ->with(['class' => 'text-blue-500 body-font bg-blue-100 shadow-md', 'message' => "{$opponent->name}さんをフォローしました"]);
     }
 
     public function unFollow(Request $request)
     {
-        $user = User::find(Auth::id());
-        $user->userFollows()->detach($request->following_id);
-        $user->decrement('followings');
+        DB::beginTransaction();
 
-        $opponent = User::find($request->following_id);
-        $opponent->decrement('followers');
+        try {
+            $user = User::find(Auth::id());
+            $user->userFollows()->detach($request->following_id);
+            $user->decrement('followings');
+
+            $opponent = User::find($request->following_id);
+            $opponent->decrement('followers');
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollback();
+            Log::critical();
+        }
 
         return back()
-        ->with(['class' => 'text-red-500 body-font bg-red-100 shadow-md', 'message' => "{$opponent->name}さんをフォローから外しました"]);
+            ->with(['class' => 'text-red-500 body-font bg-red-100 shadow-md', 'message' => "{$opponent->name}さんをフォローから外しました"]);
     }
 }
